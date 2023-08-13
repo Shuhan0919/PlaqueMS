5029e954f1aaf3385038bf49221da1c02362f7a7
shuhanliuuu <shuhanliu1996@163.com>
2023-08-13 06:11:20 +0100

    add sql file

---
+++ datasets.sql
@@ -0,0 +1,3 @@
+INSERT INTO `datasets` (`dataset_id`, `name`, `position`, `description`) VALUES ('3542c8b3-224b-41ae-8598-a9eeed0f8eb0', 'Coronary Arteries University of Virginia Cohort', '', '');
+INSERT INTO `datasets` (`dataset_id`, `name`, `position`, `description`) VALUES ('42d1dde2-188f-4b71-9db2-57148376abea', 'Carotid Plaques Athero Express', '', '');
+INSERT INTO `datasets` (`dataset_id`, `name`, `position`, `description`) VALUES ('972d974a-e013-44a5-9e68-6e275f08765f', 'Carotid Plaques Vienna Cohort', '', '');
